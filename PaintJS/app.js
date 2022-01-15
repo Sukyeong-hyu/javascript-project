bb0d062b1c45c7c48105ad61638416c019836ae9
Sukyeong-hyu <zztnrudzz13@naver.com>
2022-01-15 15:52:34 +0900

    Saving Image Function in app.js

--- PaintJS/app.js
+++ PaintJS/app.js
@@ -3,12 +3,15 @@ const ctx = canvas.getContext('2d');
 const colors = document.getElementsByClassName('jsColor');
 const range = document.getElementById('jsRange');
 const mode = document.getElementById('jsMode');
+const saveBtn = document.getElementById('jsSave');
 
 const INITIAL_COLOR = '#2c2c2c';
 
 canvas.width = 700;
 canvas.height = 600;
 
+ctx.fillStyle = 'white';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
@@ -60,9 +63,21 @@ function handleModeClick(e) {
 }
 
 function handleCanvasClick() {
-  if (!filling) {
+  if (filling) {
     ctx.fillRect(0, 0, canvas.width, canvas.height);
   }
+}
+
+function handleContextMenu(e) {
+  e.preventDefault();
+}
+
+function handleSaveClick() {
+  const image = canvas.toDataURL('image/png');
+  const link = document.createElement('a');
+  link.href = image;
+  link.download = 'PaintJS_export';
+  link.click();
 }
 
 if (canvas) {
@@ -71,6 +86,7 @@ if (canvas) {
   canvas.addEventListener('mouseup', stopPainting);
   canvas.addEventListener('mouseleave', stopPainting);
   canvas.addEventListener('click', handleCanvasClick);
+  canvas.addEventListener('contextmenu', handleContextMenu);
 }
 
 Array.from(colors).forEach((color) =>
@@ -83,4 +99,8 @@ if (range) {
 
 if (mode) {
   mode.addEventListener('click', handleModeClick);
+}
+
+if (saveBtn) {
+  saveBtn.addEventListener('click', handleSaveClick);
 }
