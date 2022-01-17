263a728950dfd5741bf3c7d6bb8a40929353ceaf
Sukyeong-hyu <zztnrudzz13@naver.com>
2022-01-17 12:43:04 +0900

    Final Wave

--- WaveJS/wavegroup.js
+++ WaveJS/wavegroup.js
@@ -6,9 +6,9 @@ export class WaveGroup {
     this.totalPoints = 6;
 
     this.color = [
-      'rgba(0, 199, 235, 0.4)',
-      'rgba(0, 146, 199, 0.4)',
-      'rgba(0, 87, 158, 0.4)',
+      'rgba(255, 0, 0, 0.4)',
+      'rgba(255, 255, 0, 0.4)',
+      'rgba(0, 255, 255, 0.4)',
     ];
 
     this.waves = [];
